900144f09955a7b6c510bd5f859d87c06e53370c
Libes6 <fya-07@list.ru>
2022-02-16 21:34:05 +0700

    onClick

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@
 import './App.css';
 
 function App() {
-  
+  let aut =()=>{
+    alert("Ты гей!");
+  }
   return (
     <div class="wrapper">
     
@@ -16,7 +18,7 @@ function App() {
     <div class="form__group">
          <input type="password" class="form__input"  placeholder=" " minLength="6" maxLength="13" />
          <label for="" class="form__label">Пароль</label>
-    <button class="form__button">Вход</button>
+    <button class="form__button" onClick={aut}>Вход</button>
       </div>
      
         </div>
